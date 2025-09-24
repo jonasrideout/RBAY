ef56208230972d8527fed51ad0b25accc8f8c575
jonasrideout <jonas.rideout@gmail.com>
2025-09-24 13:34:28 -0400

    Create providers.ts

---
+++ pages/api/auth/providers.ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export default async function providers(req: any, res: any) {
+  return await handler(req, res);
+}
