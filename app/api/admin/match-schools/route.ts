adb8ecc6eccb4e1bf0bf2ed4599f19d77c2a68c0
jonasrideout <jonas.rideout@gmail.com>
2025-10-10 10:02:05 -0400

    Update route.ts

--- app/api/admin/match-schools/route.ts
+++ app/api/admin/match-schools/route.ts
@@ -193,7 +193,7 @@ async function matchGroupWithSchool(groupId: string, schoolId: string) {
     }),
     prisma.school.update({
       where: { id: schoolId },
-      data: { matchedWithSchoolId: null }
+      data: { matchedWithSchoolId: `group:${groupId}` }
     })
   ]);
   
